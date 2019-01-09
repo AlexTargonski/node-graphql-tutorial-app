48385962d14196163812bd1d299d721457617ee6
Oleksandr <targonsky97@gmail.com>
2019-01-09 11:44:44 +0200

    add schema and resolver

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-import express    from 'express';
-import bodyParser from 'body-parser';
+import express         from 'express';
+import bodyParser      from 'body-parser';
+import graphqlHttp     from 'express-graphql';
+import { buildSchema } from 'graphql';
 
 const PORT = 8080;
 const app  = express();
 
 app.use(bodyParser.json());
+
+app.use(
+  '/graphiql', 
+  graphqlHttp({
+  schema    : buildSchema(`
+     type RootQuery {
+       events: [String!]!
+      }
+      type RootMutation {
+        createEvent(name: String): String
+      }
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue : {
+        events : () => {
+          return ['event1', 'event2'];
+        },
+        createEvent : (args) => {
+          const eventName = args.name;
+          return eventName;
+        }
+      },
+    graphiql  : true
+}));
 
 app.listen(PORT, () => console.log(`Server Now Running On localhost:${PORT}/graphiql`));
